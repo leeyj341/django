1cbf83d717892c09c8426ce66c07454bbbb6d704
leeyj341 <leeyj3794@gmail.com>
2020-06-13 17:47:54 +0900

    프로젝트 실습하기

---
+++ day05/articles/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request) :
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    context = {
+        'title' : title,
+        'content' : content
+    }
+    
+    return render(request, 'articles/index.html', context)
+
+def new_write(request) :
+    return render(request, 'articles/new.html')
+
+def create(request) :
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    context = {
+        'title' : title,
+        'content' : content
+    }
+    
+    return render(request, 'articles/create.html', context)
